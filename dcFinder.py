fdc50428d8475f25826e52f09d5f687888ce2772
Metin Yunus Kandemir <76125965+passtheticket@users.noreply.github.com>
2021-01-05 18:44:02 +0300

    Update dcFinder.py

--- dcFinder.py
+++ dcFinder.py
@@ -60,7 +60,7 @@ class service_query:
 
         except OSError:
             print(Fore.RED + Style.BRIGHT + '[-] Domain name was not resolved.')
-            print(Fore.RED + Style.BRIGHT + '[-] Check your configuration which DNS server IP address of %s domain is set, please.' % domain)
+            print(Fore.RED + Style.BRIGHT + '[-] Check your configuration file which DNS server IP address of %s domain is set, please.' % domain)
             sys.exit(Fore.RED + '[-] One bad day!')
         except TypeError:
             print(Fore.RED + Style.BRIGHT + '[-] IP address of name server is not detected.')
